6875220cdebe0d9d527a0b23e7ca1a399bfaecc2
AndresDamelio <andresdamelio24@gmail.com>
2019-05-06 02:03:20 -0400

    Agregando bandera

--- 2 - Componentes en Vuejs/ejemplo-clima/index.js
+++ 2 - Componentes en Vuejs/ejemplo-clima/index.js
@@ -9,6 +9,10 @@ Vue.component('datos-clima',{
         },
         clima:{
             type:Object
+        },
+        codigo:{
+            type: String,
+            require:true
         }
     },
     template:'#clima-ciudad'
@@ -23,12 +27,14 @@ new Vue({
         nombre: '',
         busqueda:false,
         temperatura: {},
-        clima:{}
+        clima:{},
+        codigo:''
     },
     methods: {
         consultar(){
             axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.nombre}&appid=c788c2e15f9a3bfcc8544f2ff8dc1373&units=metric&lang=es`)
                 .then(res => {
+                    this.codigo = res.data.sys.country;
                     this.temperatura = res.data.main;
                     this.clima = res.data.weather[0];
                     this.busqueda = true;
