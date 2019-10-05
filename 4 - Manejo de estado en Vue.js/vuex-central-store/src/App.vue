55377d04f55fc62cf5c610dc2568d2f955de4907
Andres Damelio <andresdamelio24@gmail.com>
2019-10-04 21:50:12 -0400

    vuex

---
+++ 4 - Manejo de estado en Vue.js/vuex-central-store/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+      <contador :cantidad="cantidad"
+                @aumentar="cantidad++"
+                @reducir="cantidad--">
+
+      </contador>
+  </div>
+</template>
+
+<script>
+import Contador from './components/Contador.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Contador
+  },
+  data() {
+    return {
+      cantidad: 0,
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
