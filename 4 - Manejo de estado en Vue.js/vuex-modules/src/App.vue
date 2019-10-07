46179e8b6538231d657bbc7ccb85adac53fb4c51
Andres Damelio <andresdamelio24@gmail.com>
2019-10-07 08:22:51 -0400

    estructura por modulos vuex

---
+++ 4 - Manejo de estado en Vue.js/vuex-modules/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <productos></productos>
+    <carro></carro>
+  </section>
+</template>
+
+<script>
+
+import Carro from './components/Carro';
+import Productos from './components/Productos';
+
+export default {
+  components:{
+    Carro, Productos
+  }
+}
+</script>
---
+++ 4 - Manejo de estado en Vue.js/vuex-modules/src/components/Carro.vue
@@ -0,0 +1,28 @@
+<template>
+    <section>
+        <h2>Carro de la compra</h2>
+        <h3>Total compra: {{ totalCompra.toFixed(2) }} $</h3>
+
+        <ul>
+            <li v-for="(producto, indice) in carro"
+                :key="indice">
+                {{ producto.nombre }}
+                <button @click="eliminarProducto(indice)">x</button>
+            </li>
+        </ul>
+
+    </section>
+</template>
+
+<script>
+
+import { mapState, mapGetters, mapMutations } from 'vuex';
+
+export default {
+    computed: {
+        ...mapGetters(['totalCompra']),
+        ...mapState(['carro'])
+    },
+    methods: mapMutations(['eliminarProducto'])
+}
+</script>
