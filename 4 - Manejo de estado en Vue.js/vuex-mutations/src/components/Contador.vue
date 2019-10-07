4aa38710f10bbdd52fcb785c98c8129c9cbba170
Andres Damelio <andresdamelio24@gmail.com>
2019-10-06 21:48:29 -0400

    formas de usar las mutaciones

--- 4 - Manejo de estado en Vue.js/vuex-mutations/src/components/Contador.vue
+++ 4 - Manejo de estado en Vue.js/vuex-mutations/src/components/Contador.vue
@@ -1,12 +1,37 @@
 <template>
     <section>
         <h1>Cantidad: {{ cantidad }}</h1>
+        <button @click="reducir">Reducir 3</button>
+        <button @click="aumentar">Aumentar 5</button>
     </section>
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { mapState, mapMutations } from 'vuex';
 export default {
-    computed: mapState(['cantidad'])
+    computed: mapState(['cantidad']),
+    //methods: {
+        // aumentar(){
+            /* Forma sencilla */
+            /* this.$store.state.cantidad++; */
+
+            /* Usando mutaciones */
+          //  this.$store.commit('aumentar');
+        //},
+        /* reducir(){
+            this.$store.commit('reducir');
+        } */
+    //},
+    /* methods: mapMutations(['aumentar', 'reducir']) */
+
+    /* Pasando informacion adicional */
+    methods: {
+        aumentar(){
+            this.$store.commit('aumentar',5)
+        },
+        reducir(){
+            this.$store.commit('reducir',3)
+        }
+    },
 }
 </script>
--- 4 - Manejo de estado en Vue.js/vuex-mutations/src/store/store.js
+++ 4 - Manejo de estado en Vue.js/vuex-mutations/src/store/store.js
@@ -6,5 +6,10 @@ Vue.use(Vuex);
 export const store = new Vuex.Store({
     state: {
         cantidad: 0,
+    },
+    mutations:{
+        /* Las mutaciones ademas de recibir el state, pueden recibir un payload */
+        aumentar: ( state, n ) => state.cantidad += n,
+        reducir: ( state, n ) => state.cantidad -= n
     }
 });
